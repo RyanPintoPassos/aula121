5e233468b6c6f74fb2eabbf4ad3e2dffc5047e55
RyanPintoPassos <119443198+RyanPintoPassos@users.noreply.github.com>
2023-08-02 14:49:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    //video.hide();
+    video.hide();
     classifier = ml5.imageClassifier('MobileNet',modelLoaded);
   }
   
